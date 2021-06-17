6208dd2fb22250c6f0e44ee84a37f3decca64de6
jocelyn vallade <jvalalde@mac.com>
2021-06-17 14:11:10 +0200

    Archiver initiale du module loginProjet

---
+++ Pages/Services/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace loginProjet.Services
+{
+    public class AppData
+    {
+       private bool canlogin;
+
+        public bool CanLogin
+        {
+            get { return canlogin; }
+            set {  canlogin=value; }
+        }
+    }
+}
---
+++ Shared/CustomAuthStateProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security;
+using System.Security.Claims;
+namespace loginProjet
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly Blazored.LocalStorage.ILocalStorageService _localStorage;
+        public CustomAuthStateProvider(Blazored.LocalStorage.ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+            string username = await _localStorage.GetItemAsync<string>("username");
+            if (!string.IsNullOrEmpty(username))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                   new System.Security.Claims.Claim(ClaimTypes.Name,username)
+                },"Test authentif");
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
+        }
+    }
+}
